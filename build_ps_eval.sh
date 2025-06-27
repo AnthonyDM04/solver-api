56dd0bfba6f31dc774a1e1ba3c1199eedcfc7c13
Anthony Morris <anthonymorris@Anthonys-iMac.local>
2025-06-27 01:05:39 +0100

    Fix build_ps_eval.sh to clone PokerStove before building

--- build_ps_eval.sh
+++ build_ps_eval.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+# Clone PokerStove repo
+git clone https://github.com/andrewprock/pokerstove.git 
+pokerstove-master
+
+# Build it
 cd pokerstove-master
-mkdir -p build
-cd build
+mkdir -p build && cd build
 cmake ..
 make -j4
-cp bin/ps-eval ../..
+
+# Copy binary to root so Flask can use it
+cp bin/ps-eval ../../ps-eval
 
